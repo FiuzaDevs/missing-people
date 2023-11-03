e58a2c952177b28a66b07ad3ce2d803cf23d6dbd
Pedro Fiuza <pedrofiuza1@gmail.com>
2023-11-03 08:05:42 -0300

    update share

--- src/app/pages/person-details/person-details.component.ts
+++ src/app/pages/person-details/person-details.component.ts
@@ -25,4 +25,16 @@ export class PersonDetailsComponent implements OnInit {
     this.router.navigate(['/']);
   }
 
+  shareWhatsapp(): void {
+    window.open(`https://api.whatsapp.com/send?text=Veja%20o%20desaparecido%20${this.person.nome}%20no%20site%20https://abitus.pjc.mt.gov.br/pessoas/${this.person.id}`, '_blank');
+  }
+
+  shareFacebook(): void {
+    window.open(`https://www.facebook.com/sharer/sharer.php?u=https://abitus.pjc.mt.gov.br/pessoas/${this.person.id}`, '_blank');
+  }
+
+  shareTwitter(): void {
+    window.open(`https://twitter.com/intent/tweet?text=Veja%20o%20desaparecido%20${this.person.nome}%20no%20site%20https://abitus.pjc.mt.gov.br/pessoas/${this.person.id}`, '_blank');
+  }
+
 }
